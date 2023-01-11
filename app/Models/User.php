347ebcda0349148a78871bdd2b9d3647679d2055
Penny Ho <hopenny00@gmail.com>
2023-01-11 13:33:46 +0800

    套件安裝

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use TCG\Voyager\Traits\VoyagerUser;
 
-class User extends Authenticatable
+class User extends \TCG\Voyager\Models\User
+
 {
+    use VoyagerUser;
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
