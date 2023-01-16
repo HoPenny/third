04272deec8b8cd97003b021fd9d7f6b1ed875668
wanting <wantingpobo@gamil.com>
2023-01-16 10:10:46 +0800

    替換網頁元素

--- app/Http/Controllers/TwoFiveController.php
+++ app/Http/Controllers/TwoFiveController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use App\Models\Element;
+
 class TwoFiveController extends Controller
 {
     public function team()
     {
-        return view('team');
+        $slider = Element::where('page', 'index')->where('position', 'slider')->orderBy('id', 'asc')->first();
+        $counters = Article::where('author_id', 1)->orderBy('sort', 'asc')->take(4)->get();
+        $teams = Article::where('author_id', 2)->orderBy('sort', 'asc')->take(3)->get();
+        $images = Element::where('page', 'index')->where('position', 'images')->orderBy('id', 'asc')->get();
+
+        return view('team', compact('slider', 'counters', 'teams', 'images'));
     }
 }
